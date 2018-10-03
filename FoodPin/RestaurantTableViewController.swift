b4546389a73273f89cfb1437f5604b04395f9a34
Andres Rambar <rambardeveloper@gmail.com>
2018-10-03 17:38:08 -0400

    search bar

--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -9,9 +9,13 @@
 import UIKit
 import CoreData
 
-class RestaurantTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+class RestaurantTableViewController: UITableViewController, NSFetchedResultsControllerDelegate, UISearchResultsUpdating {
+    
+    var searchController:UISearchController!
     
     var restaurants:[RestaurantMO] = []
+    
+    var searchResults:[RestaurantMO] = []
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent // .default
@@ -46,6 +50,16 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
         }
         tableView.estimatedRowHeight = 150
         tableView.rowHeight = UITableViewAutomaticDimension
+        
+        searchController = UISearchController(searchResultsController: nil)
+        tableView.tableHeaderView = searchController.searchBar
+        
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search restaurants"
+        searchController.searchBar.tintColor = UIColor.white
+        searchController.searchBar.barTintColor =  UIColor(red: 218.0/255.0, green:
+            100.0/255.0, blue: 70.0/255.0, alpha: 1.0)
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -55,6 +69,29 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
     
     func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         tableView.beginUpdates()
+    }
+    
+    func filterContent(for searchText:String){
+        searchResults = restaurants.filter({(restaurant)->Bool in
+            if let name = restaurant.name{
+                let isMatch  = name.localizedCaseInsensitiveContains(searchText)
+                return isMatch
+            }
+            if let location = restaurant.location{
+                let isMatch = location.localizedCaseInsensitiveContains(searchText)
+                return isMatch
+            }
+            return false
+        })
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        if !(searchController.searchBar.text?.isEmpty)!{
+            if let searchText = searchController.searchBar.text{
+                filterContent(for: searchText)
+                tableView.reloadData()
+            }
+        }
     }
     
     func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
@@ -100,19 +137,25 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return restaurants.count
+        if searchController.isActive{
+            return searchResults.count
+        }else{
+            return restaurants.count
+        }
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RestaurantTableViewCell
-        cell.nameLabel.text=restaurants[indexPath.row].name
-        cell.imageCell.image = UIImage(data: restaurants[indexPath.row].image!)
+        
+        let restaurant = (searchController.isActive) ? searchResults[indexPath.row] : restaurants[indexPath.row]
+        cell.nameLabel.text=restaurant.name
+        cell.imageCell.image = UIImage(data: restaurant.image!)
         cell.imageCell.layer.cornerRadius = 30.0
         cell.imageCell.clipsToBounds = true
-        cell.locationLabel.text = restaurants[indexPath.row].location
-        cell.typeLabel.text = restaurants[indexPath.row].type
-        cell.phoneCell.text = restaurants[indexPath.row].phone
+        cell.locationLabel.text = restaurant.location
+        cell.typeLabel.text = restaurant.type
+        cell.phoneCell.text = restaurant.phone
         if(restaurants[indexPath.row].isVisited){
             cell.accessoryType = .checkmark
         }else{
@@ -128,7 +171,8 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
             if let indexPath = tableView.indexPathForSelectedRow{
                 let destinationController = segue.destination as! RestaurantDetailViewController
                 print("row: \(indexPath.row)")
-                destinationController.restaurant = restaurants[indexPath.row]
+                destinationController.restaurant = (searchController.isActive) ?
+                    searchResults[indexPath.row] : restaurants[indexPath.row]
             }
         }
     }
@@ -140,6 +184,13 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
         tableView.deleteRows(at: [indexPath], with: .fade)
     }
     
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        if searchController.isActive{
+            return false
+        }else{
+            return true
+        }
+    }
     
     override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
         //Social Sharing button
@@ -159,6 +210,7 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
         
         shareAction.backgroundColor = UIColor(red: 48.0/255.0, green: 173.0/255.0,
                                               blue: 99.0/255.0, alpha: 1.0)
+        
         
         
         //Delete buton
